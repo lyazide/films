83402708a9cf657ff00bf118ee0716287f92b45b
lyazide <47938327+lyazide@users.noreply.github.com>
2025-01-15 08:01:16 +0100

    Init of the vite project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router';
+import MovieList from './composants/MovieList';
+import MovieDetail from './composants/MovieDetail';
+import Wishlist from './composants/Wishlist';
+import { WishlistProvider } from './WishListContext.jsx';
+
+const App = () => {
+  return (
+    
+  );
+};
+
+export default App;
